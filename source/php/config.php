a6f2e49d8b38ab5314aded5db70bc3ea248c482c
Derek Nicol <1420397+derekn@users.noreply.github.com>
2024-04-09 21:31:57 -0500

    updating config to load db vars from environment

--- source/php/config.php
+++ source/php/config.php
@@ -5,10 +5,10 @@ define('CONFIG_FILE', 1);
 /**
   * =============== DB Settings =======================================
   */
-  
-define('DB_DSN', 'mysql: host=ftp.trytarallo.altervista.org;dbname=my_trytarallo;charset=utf8');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+
+define('DB_DSN', getenv('DB_DSN') ?? 'mysql:host=mysql;port=3306;dbname=tarallo;charset=utf8');
+define('DB_USERNAME', getenv('DB_USERNAME') ?? 'tarallo');
+define('DB_PASSWORD', getenv('DB_PASSWORD') ?? '');
 
 /**
   * =============== FTP Settings ======================================
@@ -16,9 +16,9 @@ define('DB_PASSWORD', '');
 
 /**
   * Define the Tarallo root folder location for file operations.
-  * Comment this line if Tarallo is located in the ftp root.
+  * Uncomment this line if Tarallo is not located in the ftp root.
   */
-define('FTP_ROOT', '/membri/trytarallo');
+// define('FTP_ROOT', '/membri/trytarallo');
 
 /*
  * Return the correct absolute relative ftp path from a path relative to the app folder.
